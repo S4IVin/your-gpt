bae23ae79f071e2e3dcf586adda6393e63a4de88
S4lVin <salvatore.giaquinto08@gmail.com>
2023-03-08 19:45:42 +0100

    fix: 🐛 fixed text-davinci-003 model
    Fixed text-davinci-003 model, refactored the entire code, still not
    working
    
    BREAKING CHANGE: 🧨 Chat doesn't work

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import ChatMessages from '@/components/ChatMessages.vue'
 import TextArea from '@/components/TextArea.vue'
 import { ref } from 'vue'
 import { Configuration, OpenAIApi } from 'openai'
+
 const configuration = new Configuration({
-  apiKey: "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"
+  apiKey: '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'
 })
 const openai = new OpenAIApi(configuration)
 const gpt_turbo = ref(true)
@@ -13,8 +14,7 @@ const money_used = ref(0)
 const messages = ref([
   {
     role: 'system',
-    content:
-        "Sei SaGPT, un AI di Salvatore Giaquinto. Sei abbastanza concisa. Usi emoji per esprimersi"
+    content: 'Sei SaGPT, un AI di Salvatore Giaquinto. Sei abbastanza concisa. Usi emoji per esprimersi'
   }
 ])
 const clearChat = () => {
@@ -22,46 +22,50 @@ const clearChat = () => {
   tokens_used.value = 0
   money_used.value = 0
 }
-const getReply = async (text) => {
+const createConversation = async (text) => {
   if (text) {
-    messages.value.push({
-      role: 'user',
-      content: text
-    })
-    if (gpt_turbo.value) {
-      let reply = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages.value
-      })
-      tokens_used.value = reply.data.usage.total_tokens + tokens_used.value
-      money_used.value = Math.round(tokens_used.value * 0.02) / 10000
-      messages.value.push({
-        role: 'assistant',
-        content: reply.data.choices[0].message.content
-      })
-    }
-    else {
-      const roles = {'user': "Human", 'assistant': "SaGPT"}
-      let prompt = "La seguente è una conversazione con SaGPT. SaGPT è un AI di Salvatore Giaquinto. È abbastanza concisa. Usa emoji per esprimersi.\n"
-      console.log(messages.value)
-      for (let message in messages.value) {
-        console.log(message)
-        prompt = prompt + "\n" + roles[message.role] + ": " + message.content + "\n" + roles['assistant'] + ": "
-      }
-      console.log(prompt)
-      let reply = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.9
-      })
-      tokens_used.value = reply.data.usage.total_tokens + tokens_used.value
-      money_used.value = Math.round(tokens_used.value * 0.02) / 10000
-      messages.value.push({
-        role: 'assistant',
-        content: reply.data.choices[0].text
-      })
+    addMessage('user', text)
+    let reply = await generateReply()
+    updateUsageAndCost(reply.data.usage.total_tokens)
+    addMessage(
+      'assistant',
+      gpt_turbo.value ? reply.data.choices[0].messages.content : reply.data.choices[0].text
+    )
+  }
+}
+
+const generateReply = () => {
+  let getChatCompletion = gpt_turbo.value ? openai.createChatCompletion : openai.createCompletion
+
+  if (gpt_turbo.value) {
+    const roles = { user: 'Human', assistant: 'SaGPT' }
+    let prompt = 'La seguente è una conversazione con SaGPT. SaGPT è un AI di Salvatore Giaquinto. È abbastanza concisa. Usa emoji per esprimersi.\n'
+
+    for (let message of messages.value.slice(1)) {
+      prompt = `${prompt}\n ${roles[message.role]}: ${message.content}`
     }
   }
+
+  return getChatCompletion({
+    model: gpt_turbo.value ? 'gpt-3.5-turbo' : 'text-davinci-003',
+    messages: messages.value,
+    prompt: `${prompt}\nSaGPT: `,
+    max_tokens: 300,
+    temperature: 0.9,
+    presence_penalty: 0.7
+  })
+}
+
+const updateUsageAndCost = (tokens) => {
+  tokens_used.value = tokens + tokens_used.value
+  money_used.value = Math.round(tokens_used.value * 0.02) / 10000
+}
+
+const addMessage = (role, text) => {
+  messages.value.push({
+    role: role,
+    content: text
+  })
 }
 </script>
 
@@ -70,22 +74,32 @@ const getReply = async (text) => {
     <div class="flex justify-between bg-neutral-700 shadow-lg p-2.5">
       <h1 class="text-3xl font-bold">SaGPT</h1>
       <div>
-        <button @click="gpt_turbo = true" :class="{
-        'p-2 rounded-l-lg border-r-2': true,
-        'bg-neutral-500': ! gpt_turbo,
-        'bg-gray-500': gpt_turbo
-        }">GPT-Turbo</button>
-        <button @click="gpt_turbo = false" :class="{
-        'p-2 rounded-r-lg': true,
-        'bg-neutral-500': gpt_turbo,
-        'bg-gray-500': ! gpt_turbo
-        }">GPT-Davinci</button>
+        <button
+          @click="gpt_turbo = true"
+          :class="{
+            'p-2 rounded-l-lg border-r-2': true,
+            'bg-neutral-500': !gpt_turbo,
+            'bg-gray-500': gpt_turbo
+          }"
+        >
+          GPT-Turbo
+        </button>
+        <button
+          @click="gpt_turbo = false"
+          :class="{
+            'p-2 rounded-r-lg': true,
+            'bg-neutral-500': gpt_turbo,
+            'bg-gray-500': !gpt_turbo
+          }"
+        >
+          GPT-Davinci
+        </button>
       </div>
       <h2 class="text-2xl">{{ tokens_used }}</h2>
     </div>
   </header>
   <chat-messages class="mt-20 mb-20" :messages="messages"></chat-messages>
   <div class="fixed bottom-0 w-full bg-neutral-700 py-2.5" ref="chatContainer">
-    <text-area @clear="clearChat" @submit="(text) => getReply(text)"></text-area>
+    <text-area @clear="clearChat" @submit="(text) => createConversation(text)"></text-area>
   </div>
 </template>
